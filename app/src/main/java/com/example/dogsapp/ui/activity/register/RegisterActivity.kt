f0afd8496ccc20d147def537cabff4aa2d4f4d9b
ricar <ricardosr4@gmail.com>
2024-08-01 00:27:59 -0400

    creacion xml register ok

--- app/src/main/java/com/example/dogsapp/ui/activity/register/RegisterActivity.kt
+++ app/src/main/java/com/example/dogsapp/ui/activity/register/RegisterActivity.kt
@@ -1,12 +1,10 @@
 package com.example.dogsapp.ui.activity.register
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.dogsapp.R
 import com.example.dogsapp.databinding.ActivityRegisterBinding
+import com.example.dogsapp.ui.activity.preLogin.presenter.PreLoginActivity
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -15,5 +13,13 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        navigation()
+
+    }
+    private fun navigation(){
+        binding.backButton.setOnClickListener {
+            val intent = Intent(this, PreLoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
