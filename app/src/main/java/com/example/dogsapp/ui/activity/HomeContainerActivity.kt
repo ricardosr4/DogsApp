e256bd783e583e9f2a01c68a3ce57c919267fae1
ricar <ricardosr4@gmail.com>
2024-08-01 11:47:19 -0400

    primer commit de menu hamburguesa

---
+++ app/src/main/java/com/example/dogsapp/ui/activity/HomeContainerActivity.kt
@@ -0,0 +1,16 @@
+package com.example.dogsapp.ui.activity
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.dogsapp.R
+
+class HomeContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_home_container)
+
+    }
+}
